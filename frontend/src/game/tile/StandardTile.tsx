7c29d1d5b2667aa30d0e7626948dc8718de8ffcf
jeppzone <jeppzone@gmail.com>
2018-10-18 09:41:20 +0200

    Now fades in new coloured tiles for a smoother visualization

--- frontend/src/game/tile/StandardTile.tsx
+++ frontend/src/game/tile/StandardTile.tsx
@@ -1,3 +1,4 @@
+import * as Konva from 'konva';
 import * as React from 'react';
 import { Rect } from 'react-konva';
 import { ICoordinate } from '../game.typings';
@@ -12,6 +13,27 @@ interface IProps {
 
 export default class ColouredTile extends React.Component<IProps, any> {
 
+    public tile: Konva.Rect;
+
+    public componentDidMount() {
+        this.tile.fill(this.props.colour);
+    }
+
+    public componentDidUpdate() {
+        this.animate();
+    }
+
+    public animate() {
+        const tween = new Konva.Tween({
+            node: this.tile,
+            fill: this.props.colour,
+            duration: 0.5,
+            easing: Konva.Easings.EaseIn
+        });
+
+        tween.play();
+    }
+
     public render() {
         return(
             <Rect
@@ -19,9 +41,12 @@ export default class ColouredTile extends React.Component<IProps, any> {
                 y={this.props.coordinate.y}
                 width={this.props.width}
                 height={this.props.width}
-                fill = {this.props.colour}
                 cornerRadius = {5}
                 stroke={'black'}
+                ref={ (node: Konva.Rect) => { 
+                    if(node !== null) {
+                        this.tile = node;
+                }  } }
             />
         );
     }
